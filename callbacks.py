05035b26f4e55909ac4b52b4a98c56e8c6fb0db1
Martynas Jurkus <martynas.jurkus@gmail.com>
2019-09-05 17:24:03 +0300

    Final notes

--- callbacks.py
+++
@@ -1,7 +0,0 @@
-from tensorflow.keras.callbacks import Callback
-
-
-class CustomLogger(Callback):
-
-    def on_epoch_end(self, epoch: int, logs=None) -> None:
-        print(f"custom log {epoch}")
